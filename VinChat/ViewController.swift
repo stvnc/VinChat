d60ea9c48d28ed6d623f568de11435f71947261e
stvnc <vincentangelo96@yahoo.com>
2020-06-09 13:16:44 +0700

    Initial Commit

---
+++ VinChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VinChat
+//
+//  Created by Vincent Angelo on 09/06/20.
+//  Copyright © 2020 Vincent Angelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
